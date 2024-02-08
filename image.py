583a0479851f46738e8e195dcc089ae015ca1078
addieschroeder <128749404+addieschroeder@users.noreply.github.com>
2024-02-08 15:53:12 -0600

    Add files via upload

---
+++ image.py
@@ -0,0 +1,39 @@
+import cv2
+import mediapipe as mp
+
+# Initialize MediaPipe Hands.
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(static_image_mode=False,
+                       max_num_hands=2,
+                       min_detection_confidence=0.5,
+                       min_tracking_confidence=0.5)
+mp_draw = mp.solutions.drawing_utils
+
+# Initialize webcam capture.
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+        print("Ignoring empty camera frame.")
+        continue
+
+    # Convert the BGR image to RGB, and process it with MediaPipe Hands.
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(image)
+
+    # Draw the hand annotations on the image.
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_draw.draw_landmarks(
+                image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+    # Display the resulting frame
+    cv2.imshow('MediaPipe Hands', image)
+    if cv2.waitKey(5) & 0xFF == 27:  # Press 'ESC' to exit
+        break
+
+# Release the capture
+cap.release()
+cv2.destroyAllWindows()
